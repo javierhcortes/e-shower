4c730d7246c6ebc72a4e5a4ed060fc263f560b9b
Javier-titan <javierhcortes@gmail.com>
2020-01-04 22:57:29 -0300

    mejora nombre variables
    cambia display de serial

--- medidor-flujo/medidor-flujo.ino
+++ medidor-flujo/medidor-flujo.ino
@@ -1,34 +1,28 @@
-#include <Arduino.h> 
-#include <EEPROM.h> 
-
-#define LED_BUILTIN 2
-// #define SENSOR  27
-
-const int SENSOR = D2; // variable for D2 pin
-
+#include <Arduino.h>
+#include <EEPROM.h>
 
 long currentMillis = 0;
 long previousMillis = 0;
+
 int interval = 1000;
-boolean ledState = LOW;
 float calibrationFactor = 4.5;
+float flowRate;
+
 volatile byte pulseCount;
 byte pulse1Sec = 0;
-float flowRate;
+
 unsigned int flowMilliLitres;
 unsigned long totalMilliLitres;
 
-void IRAM_ATTR pulseCounter()
-{
+const int sensorFlujo = D2; // variable for D2 pin
+
+void IRAM_ATTR pulseCounter(){
   pulseCount++;
 }
 
-void setup()
-{
+void setup(){
   Serial.begin(115200);
-
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(SENSOR, INPUT_PULLUP);
+  pinMode(sensorFlujo, INPUT_PULLUP);
 
   pulseCount = 0;
   flowRate = 0.0;
@@ -36,14 +30,13 @@ void setup()
   totalMilliLitres = 0;
   previousMillis = 0;
 
-  attachInterrupt(digitalPinToInterrupt(SENSOR), pulseCounter, FALLING);
+  attachInterrupt(digitalPinToInterrupt(sensorFlujo), pulseCounter, FALLING);
 }
 
 void loop()
 {
   currentMillis = millis();
   if (currentMillis - previousMillis > interval) {
-    
     pulse1Sec = pulseCount;
     pulseCount = 0;
 
@@ -62,18 +55,18 @@ void loop()
 
     // Add the millilitres passed in this second to the cumulative total
     totalMilliLitres += flowMilliLitres;
-    
+
     // Print the flow rate for this second in litres / minute
     Serial.print("Flow rate: ");
-    Serial.print(int(flowRate));  // Print the integer part of the variable
-    Serial.print("L/min");
+    Serial.print(int(flowMilliLitres));  // Print the integer part of the variable
+    Serial.print("mL/min");
     Serial.print("\t");       // Print tab space
 
     // Print the cumulative total of litres flowed since starting
     Serial.print("Output Liquid Quantity: ");
     Serial.print(totalMilliLitres);
-    Serial.print("mL / ");
-    Serial.print(totalMilliLitres / 1000);
-    Serial.println("L");
+    Serial.println("mL");
+    // Serial.print(totalMilliLitres / 1000);
+    // Serial.println("L");
   }
 }
