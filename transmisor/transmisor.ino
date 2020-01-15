f77031f0dae1c5e727b34fe1d27b33eeaa7b9f9e
Javier-titan <javierhcortes@gmail.com>
2020-01-14 23:26:40 -0300

    cambia libreria de texto y lee sensor en D5

--- transmisor/transmisor.ino
+++ transmisor/transmisor.ino
@@ -12,9 +12,38 @@ Adafruit_SSD1306 display = Adafruit_SSD1306(128, 32, &Wire);
 const char *ssid = "poopssid";
 const char *password = "pingu4prez";
 
+long currentMillis = 0;
+long previousMillis = 0;
+
+int interval = 300;
+float calibrationFactor = 4.5;
+float flowRate;
+
+volatile byte pulseCount;
+byte pulse1Sec = 0;
+
+unsigned int flowMilliLitres;
+unsigned long totalMilliLitres;
+
+const int sensorFlujo = D5;
+
+void IRAM_ATTR pulseCounter(){
+  pulseCount++;
+}
+
+
 void setup() {
   Serial.begin(115200);
   while (!Serial) continue;
+  pinMode(sensorFlujo, INPUT_PULLUP);
+
+  pulseCount = 0;
+  flowRate = 0.0;
+  flowMilliLitres = 0;
+  totalMilliLitres = 0;
+  previousMillis = 0;
+
+  attachInterrupt(digitalPinToInterrupt(sensorFlujo), pulseCounter, FALLING);
 
   Serial.println("OLED init...");
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
@@ -47,7 +76,6 @@ void setup() {
   display.println("Inicio...");
   display.display(); // actually display all of the above
   
-
   // Explicitly set the ESP8266 to be a WiFi-client
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
@@ -63,51 +91,56 @@ void setup() {
 
     delay(300);    
   }
-
-//  u8g2.begin();
-//  u8g2.setFont(u8g2_font_logisoso62_tn);
-//  u8g2.setFontMode(0);    // enable transparent mode, which is faster
 }
 
 void loop() {
-//  // read the analog in value:
-//  sensorValue = analogRead(A0);
-//  // map to range. The pot goes from about 3 to 1023. This makes the sent value be between 0 and 999 to fit on the OLED
-//  outputValue = map(sensorValue, 3, 1023, 0, 999);
-//
-//  char intToPrint[5];
-//  itoa(outputValue, intToPrint, 10); //integer to string conversion for OLED library
-//  u8g2.firstPage();
-//  u8g2.drawUTF8(0, 64, intToPrint);
-//  u8g2.nextPage();
-//
-//  // Use WiFiClient class to create TCP connections
-//  WiFiClient client;
-//  const char * host = "192.168.4.1";
-//  const int httpPort = 80;
-//
-//  if (!client.connect(host, httpPort)) {
-//    Serial.println("connection failed");
-//    return;
-//  }
-//
-//  // We now create a URI for the request. Something like /data/?sensor_reading=123
-//  String url = "/data/";
-//  url += "?sensor_reading=";
-//  url += intToPrint;
-//
-//  // This will send the request to the server
-//  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-//               "Host: " + host + "\r\n" +
-//               "Connection: close\r\n\r\n");
-//  unsigned long timeout = millis();
-//  while (client.available() == 0) {
-//    if (millis() - timeout > 5000) {
-//      Serial.println(">>> Client Timeout !");
-//      client.stop();
-//      return;
-//    }
-//  }
-//
-//  delay(200);
+  currentMillis = millis();
+  if (currentMillis - previousMillis > interval) {
+    pulse1Sec = pulseCount;
+    pulseCount = 0;
+
+    flowRate = ((1000.0 / (millis() - previousMillis)) * pulse1Sec) / calibrationFactor;
+    previousMillis = millis();
+
+    flowMilliLitres = (flowRate / 60) * 1000;
+    totalMilliLitres += flowMilliLitres;
+
+    Serial.print("Flow rate: ");
+    Serial.print(int(flowMilliLitres));  // Print the integer part of the variable
+    Serial.print("mL/min");
+    Serial.print("\t");       // Print tab space
+
+    // Print the cumulative total of litres flowed since starting
+    Serial.print("Output Liquid Quantity: ");
+    Serial.print(totalMilliLitres);
+    Serial.println("mL");
+
+    // Use WiFiClient class to create TCP connections
+    WiFiClient client;
+    const char * host = "192.168.4.1";
+    const int httpPort = 80;
+
+    if (!client.connect(host, httpPort)) {
+      Serial.println("connection failed");
+      return;
+    }
+
+    // We now create a URI for the request. Something like /data/?sensor_reading=123
+    String url = "/data/";
+    url += "?sensor_reading=";
+    url += 50;
+
+    // This will send the request to the server
+    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+                "Host: " + host + "\r\n" +
+                "Connection: close\r\n\r\n");
+    unsigned long timeout = millis();
+    while (client.available() == 0) {
+      if (millis() - timeout > 5000) {
+        Serial.println(">>> Client Timeout !");
+        client.stop();
+        return;
+      }
+    }
+  }
 }
